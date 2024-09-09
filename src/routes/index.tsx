93499a2539b7d544e410892fb66cd71c99c03427
nvmttq <nvmttq@gmail.com>
2024-09-09 14:10:51 +0700

    remove route remain

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,9 @@ import {
 // import { locales } from "@/config";
 
 import Home from "@/pages/default/home";
+import Thang from "@/pages/default/Thang";
 import NotFound from "@/pages/NotFound";
+
 export enum ERolePath {
   ADMIN = 2,
   USER = 1,
@@ -37,6 +39,10 @@ export const createRoute = (
 
 export const router = [
   createRoute("/", <Home />, ERolePath.USER),
+  {
+    path: "/thang",
+    element: <Thang />,
+  },
   {
     path: "*",
     element: <NotFound />,
