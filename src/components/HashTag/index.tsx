3ff14776594c662dd74cb1b1d88e74cc21a32b2c
hophuoc4so9 <2224802010872@student.tdmu.edu.vn>
2024-09-13 23:02:00 +0700

    delete enum

--- src/components/HashTag/index.tsx
+++ src/components/HashTag/index.tsx
@@ -3,22 +3,6 @@ import { cva, VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/cn";
 
-// export enum ETags {
-//   CITY = "city",
-//   MAP = "map",
-//   WEATHER = "weather",
-//   DANCE = "dance",
-//   AVATAR = "avatar",
-//   TEAM = "team",
-//   ANIMAL = "animal",
-//   ABSTRACT = "abstract",
-//   MUSIC = "music",
-//   SPORT = "sport",
-//   TECHNOLOGY = "technology",
-//   CAR = "car",
-//   FOOD = "food",
-// }
-
 const HashtagVariants = cva(
   "rounded-[20px] max-h-[48px] overflow-hidden bg-accent-gray flex",
   {
