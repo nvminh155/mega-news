18bf8313b345ee1f901f1b99df0654c16f18cf7d
kiet7732 <kiet7732@gmail.com>
2024-09-17 23:20:18 +0700

    ad aboutUsd

--- src/pages/default/Kiet/index.tsx
+++ src/pages/default/Kiet/index.tsx
@@ -1,13 +1,25 @@
-import LeftSingle from "../../../components/LeftSingle/LeftSingle"
-
-
+import Explanation from "@/components/AboutUs/Explanation"
+import postData from "@/components/AboutUs/explanationData.json"
+import Contact from "@/components/AboutUs/Contact"
+import Team from "@/components/AboutUs/Team"
 
 export const Kiet = () => {
 
+    const post = postData;
     return (
         <>
-            <LeftSingle />
+            <div className="">
+                <Explanation
+                    title={post.title}
+                    content={post.content}
+                    imageUrl={post.imageUrl}
+                    alt={post.alt}
+                />
+
+                <Contact />
+                <Team />
+            </div>
+
         </>
     );
 };
- 
